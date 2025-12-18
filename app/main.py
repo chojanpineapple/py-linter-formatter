7ae1f5bf884799c5a39817d87a7af7c30e40e823
Johan Rangel <johan.apr99@gmail.com>
2025-12-18 17:37:54 +0000

    my_task

--- app/main.py
+++ app/main.py
@@ -1,26 +1,26 @@
 def format_linter_error(error: dict) -> dict:
-   
-   return {
-       "line" : error["line_number"],
-        "column" : error["column_number"],
-        "message" : error["text"],
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
         "name": error["code"],
-        "source": error["filename"]
-        }
+        "source": error["filename"],
+    }
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
-    return { "errors" : [
-                format_linter_error(error)
-            for item in errors],
-            "path": 'flake8', 
-            "status": "failed" if item else 'passed'
-        }
+    return {
+        "errors": [
+            format_linter_error(error)
+            for error in errors
+        ],
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-
     return [
-        format_single_linter_file(file_path, errors)
+        format_single_linter_file(path, values)
         for path, values in linter_report.items()
     ]
