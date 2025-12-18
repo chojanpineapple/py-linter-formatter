49d6fa93255a9b77a5b6149d30847db1a52b97d3
Johan Rangel <johan.apr99@gmail.com>
2025-12-18 17:45:32 +0000

    my_task

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8",
     }
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
 
     return {
@@ -20,9 +19,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "failed" if errors else "passed",
     }
 
-
 def format_linter_report(linter_report: dict) -> list:
-    
+
     return [
         format_single_linter_file(path, values)
         for path, values in linter_report.items()
