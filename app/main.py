53a5750704dfa58f7af4ddd13bf48d92276c0d24
Johan Rangel <johan.apr99@gmail.com>
2025-12-18 17:25:14 +0000

    my_task

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def format_linter_error(error: dict) -> dict:
+   
    return {
        "line" : error["line_number"],
         "column" : error["column_number"],
@@ -8,6 +9,7 @@ def format_linter_error(error: dict) -> dict:
         }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
+
     return { "error" : [
                 {
                 "line" : item["line_number"],
@@ -22,6 +24,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
+    
     return [
         {
             "errors": [
